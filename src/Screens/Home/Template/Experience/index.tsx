844b950ca958fded5802a920340cea1771935327
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2024-02-13 17:05:13 -0300

    corrigindo bug: removendo lib não utilizada

--- src/Screens/Home/Template/Experience/index.tsx
+++ src/Screens/Home/Template/Experience/index.tsx
@@ -8,22 +8,6 @@ import {
 import { experienceData } from '../../../../Services/experienceData'
 import ExperienceCard from '../../Components/ExperienceCard'
 
-import { Chrono } from "react-chrono";
-
-const items = [{
-  title: "May 1940",
-  cardTitle: "Dunkirk",
-  url: "http://www.history.com",
-  cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-  cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-  media: {
-    type: "IMAGE",
-    source: {
-      url: "http://someurl/image.jpg"
-    }
-  }
-}];
-
 const Experience: React.FC = () => {
 
   return (
