0afb97526e80acef33062eedfa41c64a3861ee92
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2024-08-23 15:05:12 -0300

    atualizando foto de perfil

--- src/Screens/Home/Components/ContactBadges/index.tsx
+++ src/Screens/Home/Components/ContactBadges/index.tsx
@@ -13,18 +13,18 @@ const ContactBadges: React.FC = () => {
             <Logo href='https://github.com/victorluis22' target={'_blank'}>
             <IoLogoGithub />
             </Logo>
-            <Logo href='https://www.linkedin.com/in/victor-luis-teixeira-reis-877361243/' target={'_blank'}>
+            <Logo href='https://www.linkedin.com/in/victor-luis-teixeira-reis' target={'_blank'}>
             <IoLogoLinkedin />
-            </Logo>
-            <Logo href='https://www.instagram.com/victor_luis22/' target={'_blank'}>
-            <IoLogoInstagram />
+            {/* </Logo>
+            <Logo href='https://www.instagram.com/victor_luis22' target={'_blank'}>
+            <IoLogoInstagram /> */}
             </Logo>
             <Logo href='https://mail.google.com/mail/?view=cm&fs=1&to=victorluis2002@gmail.com' target={'_blank'}>
             <IoMail />
             </Logo>
-            <Logo href='https://wa.me/5522998779458' target={'_blank'}>
+            {/* <Logo href='https://wa.me/5522998779458' target={'_blank'}>
             <IoLogoWhatsapp />
-            </Logo>
+            </Logo> */}
         </Container>
     )
 }
